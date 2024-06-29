b803db891526cf42afc137f92f337c8f17760cc4
DePacos <depacos@yandex.ru>
2024-06-29 21:29:34 +0300

    added file

---
+++ JavaScript/7 kyu/FindingLengthOfTheSequence.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+//   As part of this kata, you need to find the length of the sub-array that begins and ends with the specified number.
+//
+//   For example, if the array arr is [0, -3, 7, 4, 0, 3, 7, 9], finding the length of the sub-array that begins and ends with 7s would return 5.
+//
+//   For sake of simplicity, there will only be numbers (positive or negative) in the supplied array.
+//
+//   If there are less or more than two occurrences of the number to search for, return 0.
+
+
+
+var lengthOfSequence = function (arr, n) {
+  const arrSort = [...arr].sort()
+  const index = arrSort.indexOf(n)
+
+  if(arr.indexOf(n) === arr.lastIndexOf(n) || arrSort[index] === arrSort[index + 2]) return 0
+
+  return arr.slice(arr.indexOf(n), arr.lastIndexOf(n) + 1).length
+};
