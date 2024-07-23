3e9ea705222766e9a42f4e30dd4f50a45031009c
DePacos <depacos@yandex.ru>
2024-07-23 23:10:28 +0300

    added file

---
+++ JavaScript/6 kyu/ReverseOrRotate.js
@@ -0,0 +1,42 @@
+// DESCRIPTION:
+//   The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+//
+// If the sum of a chunk's digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+//
+// If
+//
+// sz is <= 0 or if str == "" return ""
+// sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+//   Examples:
+// ("123456987654", 6) --> "234561876549"
+// ("123456987653", 6) --> "234561356789"
+// ("66443875", 4) --> "44668753"
+// ("66443875", 8) --> "64438756"
+// ("664438769", 8) --> "67834466"
+// ("123456779", 8) --> "23456771"
+// ("", 8) --> ""
+// ("123456779", 0) --> ""
+// ("563000655734469485", 4) --> "0365065073456944"
+// Example of a string rotated to the left by one position:
+//   s = "123456" gives "234561".
+
+
+
+function revrot(str, sz) {
+  if(sz <= 0 || str.length === '' || str.length < sz) return ''
+
+  let arr = str.split('')
+  let result = ''
+
+  for(let i = 0; i < Math.floor(str.length / sz); i++){
+    let spliceStr = arr.splice(0, sz)
+    let sum = spliceStr.reduce((acc, current) => acc+= +current, 0)
+
+    if(sum % 2 === 0){
+      result += spliceStr.reverse().join('')
+    }else{
+      result += spliceStr.slice(1).join('') + spliceStr[0]
+    }
+  }
+  return result
+}
