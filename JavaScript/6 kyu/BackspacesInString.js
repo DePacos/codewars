139127b743b33d123e7abd4596b8203696a45aca
DePacos <depacos@yandex.ru>
2024-07-28 22:49:06 +0300

    added file

---
+++ JavaScript/6 kyu/BackspacesInString.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+//   Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+//
+// Your task is to process a string with "#" symbols.
+//
+//   Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+// ""              ==>  ""
+
+
+
+function cleanString(s) {
+  let result = []
+  for(i = 0; i < s.length; i++){
+    s[i] !== '#' ? result.push(s[i]) : result.pop()
+  }
+  return result.join('')
+}
